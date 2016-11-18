f4387a0fdb2883775f926b4054d6d5621e3432e4
Glaadiss <bartekgladys@gmail.com>
2016-11-18 12:42:28 +0100

    new colors

--- main.cpp
+++ main.cpp
@@ -37,17 +37,16 @@ int main()
         }
     };
 
-
     std::vector<point> points;
     std::vector<sf::CircleShape> circles;
     bool Q[100];
     bool S[100];
-    for(int i =0; i<10; i++){
-        for(int g =0; g<10; g++){
+    for(int i =0; i<10; ++i){
+        for(int g =0; g<10; ++g){
             sf::CircleShape circle;
             circle.setRadius(r);
             circle.setPosition(r * g * 4 + 30, r * i * 4 + 30);
-            circle.setOutlineColor(sf::Color::White);
+            circle.setOutlineColor(sf::Color::Red);
             circle.setFillColor(sf::Color::White);
             circles.push_back(circle);
             int iter = i * 10 + g;
@@ -58,9 +57,8 @@ int main()
         }
     }
 
-    for( int i =1; i<100; i++ ){
-
-        if ( i%10 != 0  && rand()%9 > 2 ){
+    for( int i =1; i<100; ++i ){
+        if ( i%10 != 0  && rand()%9 > 3 ){
             points[i-1].right = 1;
             points[i].left = 1;
             lines_array[i][0] = sf::Vertex(sf::Vector2f( circles[i-1].getPosition().x + r, circles[i-1].getPosition().y + r ));
@@ -72,8 +70,8 @@ int main()
             values[i] = 999;
         }
     }
-    for( int i =0; i<90; i++ ){
-        if ( rand()%9 > 2 ){
+    for( int i =0; i<90; ++i ){
+        if ( rand()%9 > 3 ){
             points[i].bot = 1;
             points[i + 10].top = 1;
             lines_array[i + 100][0] = sf::Vertex(sf::Vector2f( circles[i].getPosition().x + r, circles[i].getPosition().y + r ), sf::Color::White);
@@ -85,10 +83,7 @@ int main()
             values [i +100] = 999;
         }
     }
-
     points[0].weight = 0;
-
-
     for( auto i : points){
         point bufor;
         bufor.weight = 9999999;
@@ -99,7 +94,7 @@ int main()
         Q[bufor.id] = false;
         S[bufor.id] = true;
         int cords[4] = { 10, -1, -10, 1 };
-        for(int j =0; j < 4; j++){
+        for(int j =0; j < 4; ++j){
             if( bufor.id -cords[j] < 100 && bufor.id - cords[j] >= 0 && Q[bufor.id -cords[j]] ){
                 if( points[bufor.id - cords[j]].weight > bufor.weight + bufor.get_direction(j) ){
                     points[bufor.id - cords[j]].weight = bufor.weight + bufor.get_direction(j) ;
@@ -108,14 +103,6 @@ int main()
             }
         }
     }
-
-
-
-    for(int i =0; i < 25; i++){
-        //std::cout << points[i].id << " - " << points[i].weight << " - " << points[i].related_id  << std::endl;
-        //std::cout << points[0].get_direction(3) << std::endl;
-    }
-
     int point_id = 0;
 
     while (app.isOpen())
@@ -131,17 +118,17 @@ int main()
         // Clear screen
         app.clear();
         // Draw the sprite
-        for(auto circle : circles){
-            app.draw(circle);
-        }
         for(auto line : lines_array){
             app.draw(line, 2, sf::Lines);
         }
-
+        for(auto circle : circles){
+            app.draw(circle);
+        }
+        // Pick finish circle
         if (event.type == sf::Event::MouseButtonPressed){
             if (event.mouseButton.button == sf::Mouse::Left)
             {
-                for(int i=0; i < circles.size(); i++){
+                for(int i=0; i < circles.size(); ++i){
                     if(circles[i].getPosition().x <= event.mouseButton.x && circles[i].getPosition().x + 20 >= event.mouseButton.x){
                         if(circles[i].getPosition().y <= event.mouseButton.y && circles[i].getPosition().y + 20 >= event.mouseButton.y){
                             point_id = i;
@@ -150,11 +137,11 @@ int main()
                 }
             }
         }
-        for(int i =0; i < circles.size(); i++){
+        for(int i =0; i < circles.size(); ++i){
             circles[i].setFillColor(sf::Color::White);
         }
         while( point_id > -1){
-            circles[point_id].setFillColor(sf::Color::Green);
+            circles[point_id].setFillColor(sf::Color::Red);
             point_id = points[point_id].related_id;
         }
 
